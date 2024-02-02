ff2ab02dd45b214628ecb0e59fdaf1cd44df8a15
BHANURI CHANDRASEKHAR <133088092+BHANURICHANDRASEKHAR@users.noreply.github.com>
2024-02-03 00:09:13 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
 app.use(
     cors({
-      origin: ['http://localhost:5173'],
+      origin: ['https://final-eco-mart-final.vercel.app'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       exposedHeaders: ['Content-Type', 'Authorization', 'XMLHttpRequest'],
       credentials: true, // Allow credentials
