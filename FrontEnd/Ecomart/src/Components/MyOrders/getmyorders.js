c478167a1c21c5838a3aae390c90852f71ee7a9c
BHANURI CHANDRASEKHAR <133088092+BHANURICHANDRASEKHAR@users.noreply.github.com>
2024-02-13 14:04:05 +0530

    Update getmyorders.js

--- FrontEnd/Ecomart/src/Components/MyOrders/getmyorders.js
+++ FrontEnd/Ecomart/src/Components/MyOrders/getmyorders.js
@@ -1,8 +1,9 @@
 import { getcookie } from "../../../fetchfunction";
 import axios from "axios";
-const token=getcookie();
+
 export default function myordersdata(name,setordersdata)
 {
+    const token=getcookie();
     axios.get(`https://ecomart-apii.onrender.com/myorders/get?tablename=${name}`,{
         headers:{
             'x-token':token,
